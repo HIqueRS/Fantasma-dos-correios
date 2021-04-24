cd4ff7665fe602ff5b24e517067f4023323eb773
Hique <riquesouzalima11@gmail.com>
2021-04-24 20:06:42 -0300

    salvando a movimentacao

--- Fantasma dos correios/Assets/Script/PlayerMovement.cs
+++ Fantasma dos correios/Assets/Script/PlayerMovement.cs
@@ -4,15 +4,49 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    private Rigidbody2D rb2D;
+    private Vector3 dir;
+    private Vector3 rot;
+    private float vel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2D = transform.GetComponent<Rigidbody2D>();
+        dir.x = 1;
+        vel = 250f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.W))
+        {
+            Quaternion currentRotation = transform.rotation;
+            Quaternion wantedRotation = Quaternion.Euler(0, 0, 90);
+            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * vel);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Quaternion currentRotation = transform.rotation;
+            Quaternion wantedRotation = Quaternion.Euler(0, 0, 270);
+            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * vel);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Quaternion currentRotation = transform.rotation;
+            Quaternion wantedRotation = Quaternion.Euler(0, 0, 180);
+            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * vel);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Quaternion currentRotation = transform.rotation;
+            Quaternion wantedRotation = Quaternion.Euler(0, 0, 0);
+            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * vel);
+        }
+        //transform.Rotate(0, 0 , Input.GetAxis("Horizontal") * 60 * Time.deltaTime);
+
+        transform.position += transform.right * Time.deltaTime * 2.5f;
     }
 }
