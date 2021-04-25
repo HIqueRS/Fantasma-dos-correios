d360074af1f8505f697d44cfb9c15977f17136fb
Hique <riquesouzalima11@gmail.com>
2021-04-24 21:30:28 -0300

    salvando o codigo

--- Fantasma dos correios/Assets/Script/PlayerMovement.cs
+++ Fantasma dos correios/Assets/Script/PlayerMovement.cs
@@ -4,49 +4,48 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    private Rigidbody2D rb2D;
-    private Vector3 dir;
-    private Vector3 rot;
-    private float vel;
+    [SerializeField] private float _rotationVelocity = 250.0f;
+    [SerializeField] private float _Velocity = 2.5f;
 
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        rb2D = transform.GetComponent<Rigidbody2D>();
-        dir.x = 1;
-        vel = 250f;
+        
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        Input_movement();
+    }
+
+    private void Input_movement()
+    {
+        if (Input.GetKey(KeyCode.W))
         {
             Quaternion currentRotation = transform.rotation;
             Quaternion wantedRotation = Quaternion.Euler(0, 0, 90);
-            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * vel);
+            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * _rotationVelocity);
         }
         if (Input.GetKey(KeyCode.S))
         {
             Quaternion currentRotation = transform.rotation;
             Quaternion wantedRotation = Quaternion.Euler(0, 0, 270);
-            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * vel);
+            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * _rotationVelocity);
         }
         if (Input.GetKey(KeyCode.A))
         {
             Quaternion currentRotation = transform.rotation;
             Quaternion wantedRotation = Quaternion.Euler(0, 0, 180);
-            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * vel);
+            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * _rotationVelocity);
         }
         if (Input.GetKey(KeyCode.D))
         {
             Quaternion currentRotation = transform.rotation;
             Quaternion wantedRotation = Quaternion.Euler(0, 0, 0);
-            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * vel);
+            transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * _rotationVelocity);
         }
-        //transform.Rotate(0, 0 , Input.GetAxis("Horizontal") * 60 * Time.deltaTime);
 
-        transform.position += transform.right * Time.deltaTime * 2.5f;
+        transform.position += transform.right * Time.deltaTime * _Velocity;
     }
 }
