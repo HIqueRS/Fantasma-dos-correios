203f60c6cc041b7675ba24d53b29812236f176ca
Hique <riquesouzalima11@gmail.com>
2021-04-24 22:23:06 -0300

    salvando

--- Fantasma dos correios/Assets/Script/PlayerMovement.cs
+++ Fantasma dos correios/Assets/Script/PlayerMovement.cs
@@ -5,15 +5,10 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _rotationVelocity = 250.0f;
-    [SerializeField] private float _Velocity = 2.5f;
+    [SerializeField] private float _topVelocity = 2.5f;
+    [SerializeField] private float _acceleration = 1f;
+    private float _velocity;
 
-    // Start is called before the first frame update
-    private void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     private void Update()
     {
         Input_movement();
@@ -24,28 +19,33 @@ public class PlayerMovement : MonoBehaviour
         if (Input.GetKey(KeyCode.W))
         {
             Quaternion currentRotation = transform.rotation;
-            Quaternion wantedRotation = Quaternion.Euler(0, 0, 90);
+            Quaternion wantedRotation = Quaternion.Euler(0, 0, 180);
             transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * _rotationVelocity);
         }
         if (Input.GetKey(KeyCode.S))
         {
             Quaternion currentRotation = transform.rotation;
-            Quaternion wantedRotation = Quaternion.Euler(0, 0, 270);
+            Quaternion wantedRotation = Quaternion.Euler(0, 0, 0);
             transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * _rotationVelocity);
         }
         if (Input.GetKey(KeyCode.A))
         {
             Quaternion currentRotation = transform.rotation;
-            Quaternion wantedRotation = Quaternion.Euler(0, 0, 180);
+            Quaternion wantedRotation = Quaternion.Euler(0, 0, 270);
             transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * _rotationVelocity);
         }
         if (Input.GetKey(KeyCode.D))
         {
             Quaternion currentRotation = transform.rotation;
-            Quaternion wantedRotation = Quaternion.Euler(0, 0, 0);
+            Quaternion wantedRotation = Quaternion.Euler(0, 0, 90);
             transform.rotation = Quaternion.RotateTowards(currentRotation, wantedRotation, Time.deltaTime * _rotationVelocity);
         }
 
-        transform.position += transform.right * Time.deltaTime * _Velocity;
+        if(_velocity < _topVelocity)
+        {
+            _velocity += Time.deltaTime * _acceleration;
+        }
+
+        transform.position += -transform.up * Time.deltaTime * _velocity;
     }
 }
---
+++ Fantasma dos correios/Assets/Script/PlayerShoot.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] private GameObject _letter;
+    private Vector3 _shootDirection;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            
+        }
+    }
+}
